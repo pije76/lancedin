36c846c1d5ea8fa65811ad0439892c36c0500de6
Panjianom Adipratomo <panjul76@hotmail.com>
2013-05-27 07:00:56 +0700

    commit

--- lancedin/settings.py
+++ lancedin/settings.py
@@ -153,7 +153,7 @@ INSTALLED_APPS = (
     #'userena',
     #'guardian',
     'registration',
-    'profiles',
+    #'profiles',
     'tagging',
     #'pin', #pinterest
     #'daddy_avatar',
@@ -259,8 +259,8 @@ DATABASES['default'] =  dj_database_url.config()
 
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
