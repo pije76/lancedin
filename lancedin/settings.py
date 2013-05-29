fdf8143ad166061db264b36290fe654e5b5a59de
Panjianom Adipratomo <panjul76@hotmail.com>
2013-05-29 13:04:05 +0700

    commit

--- lancedin/settings.py
+++ lancedin/settings.py
@@ -158,7 +158,7 @@ INSTALLED_APPS = (
     'debug_toolbar',
     'ratings',
     'bootstrapform',
-    'userenabootstrap',
+    #'userenabootstrap',
     'userena',
     'guardian',
     #'registration',
