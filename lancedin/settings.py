e8c09323f829d224309331138a5f828945e4ee9d
Panjianom Adipratomo <panjul76@hotmail.com>
2013-06-09 01:18:31 +0700

    commit

--- lancedin/settings.py
+++ lancedin/settings.py
@@ -372,7 +372,7 @@ APPEND_SLASH = True
 # Heroku configuration #
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
