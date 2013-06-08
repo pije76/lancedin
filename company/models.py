d37ac13ba577af51d68549df2f6bef0c49381f8f
Panjianom Adipratomo <panjul76@hotmail.com>
2013-06-09 01:32:55 +0700

    commit

--- company/models.py
+++ company/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.utils.translation import ugettext_lazy as _
 
 from userena.models import UserenaBaseProfile
-from country_utils.fields import CountryField
+#from country_utils.fields import CountryField
 
 # Create your models here.
 class Profile(UserenaBaseProfile):
@@ -14,7 +14,7 @@ class Profile(UserenaBaseProfile):
     logo = models.CharField("Company Logo", max_length=80)
     website = models.URLField("Company Website", max_length=80, verify_exists=False, null=True, blank=True, help_text=_('"www.company.com"'))
     email = models.EmailField("Email", max_length=254)
-    country = CountryField(blank=False)
+#    country = CountryField(blank=False)
 
     class Meta:
         verbose_name = 'Client'
