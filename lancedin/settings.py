6628dc03ae08249d3545904d2ef977a169c92a74
Panjianom Adipratomo <panjul76@hotmail.com>
2013-06-09 01:37:28 +0700

    commit

--- lancedin/settings.py
+++ lancedin/settings.py
@@ -15,8 +15,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'lancedin.db',                      # Or path to database file if using sqlite3.
         'USER': 'pije76',                      # Not used with sqlite3.
         'PASSWORD': 'tratap60',                  # Not used with sqlite3.
